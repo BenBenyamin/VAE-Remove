447ca0e9e911d0b4b56fafc9d8a3c216809c2fb9
Ben Benyamin <ofh8750@sheep.mech.northwestern.edu>
2025-05-04 17:53:12 -0500

    Encoder seems to be done now

--- model/model.py
+++ model/model.py
@@ -14,6 +14,8 @@ class LatentGaussian(nn.Module):
 
     def __init__(self,latent_size):
 
+        super().__init__()
+
         self.latent_size = latent_size
         self.mean_layer = nn.LazyLinear(latent_size)
         self.log_var_layer = nn.LazyLinear(latent_size)
@@ -39,16 +41,16 @@ class Encoder(nn.Module):
         super().__init__()
 
         layers = [nn.Conv2d(input_channels,feature_sizes[0],kernel_size=4, stride=2,padding=1,bias=False)]
-        layers.append(nn.BatchNorm2d())
+        layers.append(nn.BatchNorm2d(feature_sizes[0]))
         layers.append(nn.LeakyReLU())
         
         for i in range(1,len(feature_sizes)):
             layers.append(nn.Conv2d(feature_sizes[i-1],feature_sizes[i],kernel_size=4, stride=2,padding=1,bias=False))
-            layers.append(nn.BatchNorm2d())
+            layers.append(nn.BatchNorm2d(feature_sizes[i]))
             layers.append(nn.LeakyReLU())
         
         layers.append(nn.Conv2d(feature_sizes[-1],latent_size,kernel_size=3, stride=1,padding=1,bias=False))
-        layers.append(nn.BatchNorm2d())
+        layers.append(nn.BatchNorm2d(latent_size))
 
         self.layers = nn.Sequential(*layers)
 
@@ -60,7 +62,6 @@ class Encoder(nn.Module):
     def forward(self,x):
 
         x =  self.layers(x)
-
         mean , log_var = self.gaussian(x)
 
         z = self.reparameterize(mean,log_var)
@@ -77,4 +78,5 @@ encoder = Encoder(input_channels=3, latent_size=128)
 x = torch.randn(1, 3, 120, 120)  # batch of 1, 3-channel 64x64 image
 out = encoder(x)
 print("Input shape:", x.shape)
-print("Output shape:", out.shape)
+for o in out:
+    print("Output shape:", o.shape)
